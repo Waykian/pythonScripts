fcd21978aa44eaf367c80781803b639a30b0f344
theWacian <lavignagiovanni@hotmail.it>
2022-04-21 16:10:05 +0200

    Various edits

--- RandomWikiPage/wikipediaRandom.py
+++ RandomWikiPage/wikipediaRandom.py
@@ -1,13 +1,15 @@
 import subprocess
 from bs4 import BeautifulSoup
-import os
+
+
 import sys
+print(sys.version)
 
 while True:
     check = input("Do you want to read the article? ")
     match check:
         case ('y'|'Y'):
-            p = subprocess.run(["python", "fetchRandomWikipediaPage.py"])
+            p = subprocess.run(["python", "RandomWikiPage/fetchRandomWikipediaPage.py"])
         case ('n'|'N'):
             continue
         case _:
